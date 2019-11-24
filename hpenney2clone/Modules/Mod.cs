58fa5efb2b762185610b9fe43db83307849f60c0
hpenney2 <hunter_penney@yahoo.com>
2019-11-24 15:42:48 -0600

    Added actual bot
    Added hpenney3 solution.

---
+++ hpenney2clone/Modules/Mod.cs
@@ -0,0 +1,65 @@
+﻿/*using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord.Addons.Interactive;
+
+namespace hpenney2clone.Modules
+{
+    public class Mod : ModuleBase<ShardedCommandContext>
+    {
+        [Group("mod"), RequireContext(ContextType.Guild)]
+        public class ModModule : InteractiveBase<ShardedCommandContext>
+        {
+            [Command("kick", RunMode = RunMode.Async), RequireUserPermission(Discord.GuildPermission.KickMembers), RequireBotPermission(Discord.GuildPermission.KickMembers)]
+            public async Task KickAsync(SocketGuildUser user = null, [Remainder] string reason = null)
+            {
+                if (user == null) { await ReplyAsync("i need a user to kick dumb dumb"); return; }
+                
+                if (reason == null) { await ReplyAsync($"f for {user.Mention}, they have been kicked from the server. no reason given by {Context.User.Username}."); await user.KickAsync(); return; }
+                else { await ReplyAsync($"f for {user.Mention}, they have been kicked from the server. reason given by **{Context.User.Username}**: **{reason}**"); await user.KickAsync(reason); return; }
+            }
+
+            [Command("ban", RunMode = RunMode.Async), RequireUserPermission(Discord.GuildPermission.BanMembers), RequireBotPermission(Discord.GuildPermission.BanMembers)]
+            public async Task BanAsync(SocketGuildUser user = null, [Remainder] string reason = null)
+            {
+                if (user == null) { await ReplyAsync("i need a user to ban dumb dumb"); return; }
+
+                await ReplyAsync($"ay chief, you sure you wanna ban {user.Username}? (10 seconds to choose, y/n)");
+                var response = await NextMessageAsync(true, true, TimeSpan.FromSeconds(10));
+
+                if (response != null && response.Content.ToLower() == "y")
+                {
+                    if (reason == null)
+                    {
+                        await ReplyAsync($"<a:snap:635296176022749190> mega f to {user.Mention}, they have been banned from {Context.Guild.Name}. no reason given by **{Context.User.Username}**.");
+                        var dmUser = await user.GetOrCreateDMChannelAsync();
+                        await dmUser.SendMessageAsync($"uh oh, looks like you just got banned from {Context.Guild.Name} by {Context.User.Username}. no reason given.");
+                        await BanAsync(user);
+                        return;
+                    }
+                    else
+                    {
+                        await ReplyAsync($"<a:snap:635296176022749190> mega f to {user.Mention}, they have been banned from {Context.Guild.Name}. reason given by **{Context.User.Username}**: {reason}");
+                        var dmUser = await user.GetOrCreateDMChannelAsync();
+                        await dmUser.SendMessageAsync($"uh oh, looks like you just got banned from {Context.Guild.Name} by {Context.User.Username}. reason: {reason}");
+                        await BanAsync(user, reason);
+                        return;
+                    }
+                }
+                else
+                {
+                    if (response != null && response.Content.ToLower() == "n")
+                    {
+                        await ReplyAsync($"{user.Username} has been spared");
+                        return;
+                    }
+                    else
+                        await ReplyAsync($"⌚ you took too long so I spared {user.Username}");
+                    return;
+                }
+            }
+        }
+    }
+}
+*/
---
+++ hpenney2clone/Modules/Utility.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace hpenney2clone.Modules
+{
+    public class Utility : ModuleBase<ShardedCommandContext>
+    {
+        [Command("userinfo"), Alias("uinfo", "info")]
+        [Summary("Gets information about a user in a nice embed.")]
+        public async Task GetUInfoAsync(SocketGuildUser user = null)
+        {
+            // Makes a default value for the user the bot gets info for if the command was given no user.
+            var userGet = user ?? Context.User as SocketGuildUser;
+
+            // Creates new EmbedBuilder.
+            var userInfo = new Discord.EmbedBuilder()
+            {
+                Title = $"Info about **{userGet.Username}**",
+                ThumbnailUrl = userGet.GetAvatarUrl() ?? userGet.GetDefaultAvatarUrl(),
+                Color = Discord.Color.Green
+            };
+            
+            // Adds fields containing various types of information about the user.
+            userInfo.AddField("Username & Discriminator", userGet.Username + "#" + userGet.Discriminator, true);
+            userInfo.AddField("Nickname", userGet.Nickname ?? "None", true);
+            userInfo.AddField("Account Created At", userGet.CreatedAt.DateTime.ToString(), true);
+            userInfo.AddField("Joined Server At", userGet.JoinedAt.Value.DateTime.ToString(), true);
+            if (userGet.Status != Discord.UserStatus.DoNotDisturb)
+                userInfo.AddField("Status", userGet.Status.ToString(), true);
+            else
+                userInfo.AddField("Status", "Do Not Disturb", true);
+            userInfo.AddField("Bot", userGet.IsBot ? "Yes" : "No", true);
+
+            // Posts the embed to the channel.
+            await ReplyAsync("", false, userInfo.Build());
+        }
+    }
+}
